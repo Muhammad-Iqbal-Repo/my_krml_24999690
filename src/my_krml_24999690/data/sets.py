a277ed25942aee543d2e6ad565a333c2a454093c
Muhammad-Iqbal-Repo <Muhammad.Iqbal-1@student.uts.edu.au>
2025-08-28 19:21:05 +1000

    Fix Typo

--- src/my_krml_24999690/data/sets.py
+++ src/my_krml_24999690/data/sets.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sklearn.prepocess import StandardScaler
+from sklearn.prepocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import roc_auc_score
 
